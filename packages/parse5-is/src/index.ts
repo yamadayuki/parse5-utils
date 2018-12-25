ef29caa694afbff4d72b970794d85df7f9264916
Yuki Yamada <yamadayuuki0818@gmail.com>
2018-12-25 15:04:16 +0900

    Add some type guard functions

--- packages/parse5-is/src/index.ts
+++ packages/parse5-is/src/index.ts
@@ -1,1 +1,45 @@
-console.log("Hello world");
+import {
+  DefaultTreeCommentNode,
+  DefaultTreeDocument,
+  DefaultTreeDocumentFragment,
+  DefaultTreeDocumentType,
+  DefaultTreeElement,
+  DefaultTreeNode,
+  DefaultTreeTextNode,
+} from "parse5";
+
+export function isNode(node: DefaultTreeNode): node is DefaultTreeNode {
+  return node.nodeName !== undefined && node.nodeName !== null;
+}
+
+export function isDocument(node: DefaultTreeNode): node is DefaultTreeDocument {
+  return node.nodeName === "#document";
+}
+
+export function isTextNode(node: DefaultTreeNode): node is DefaultTreeTextNode {
+  return node.nodeName === "#text";
+}
+
+export function isCommentNode(
+  node: DefaultTreeNode
+): node is DefaultTreeCommentNode {
+  return node.nodeName === "#comment";
+}
+
+export function isDocumentFragment(
+  node: DefaultTreeNode
+): node is DefaultTreeDocumentFragment {
+  return node.nodeName === "#document-fragment";
+}
+
+export function isDocumentType(
+  node: DefaultTreeNode
+): node is DefaultTreeDocumentType {
+  return node.nodeName === "#documentType";
+}
+
+export function hasSourceCodeLocation(
+  node: DefaultTreeElement | DefaultTreeTextNode
+): node is Required<typeof node> {
+  return node.sourceCodeLocation !== undefined;
+}
